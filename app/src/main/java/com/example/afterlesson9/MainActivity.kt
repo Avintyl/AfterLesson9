80dcc50e296163076d092e6aa41596b60a526fde
Nick <loriander@yandex.ru>
2022-02-24 22:28:56 +0300

    Done work

--- app/src/main/java/com/example/afterlesson9/MainActivity.kt
+++ app/src/main/java/com/example/afterlesson9/MainActivity.kt
@@ -1,11 +1,46 @@
 package com.example.afterlesson9
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.afterlesson9.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    companion object
+
+    var listOfMotivation: List<String> = listOf(
+        "You are the best student!",
+        "Per aspera ad astra",
+        "Try and try, and give reward",
+        "Good boy!",
+        "Excellent work!",
+        "Cool, bro..."
+    )
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        displayNexLabel()
+
+        binding.btnUpdateMotiveLabel.setOnClickListener {
+            displayNexLabel()
+        }
+    }
+
+    @SuppressLint("SetTextI18n")
+    private fun displayNexLabel() {
+        if (!listOfMotivation.isNullOrEmpty()) {
+            var nextLabel = listOfMotivation.random()
+            while (binding.tvMotiveLabel.text == nextLabel) {
+                nextLabel = listOfMotivation.random()
+            }
+            binding.tvMotiveLabel.text = nextLabel
+        } else binding.tvMotiveLabel.text = "No motivation for you, sorry..."
     }
 }
